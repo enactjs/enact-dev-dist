4b07a66369328a5a9b65ee2cfcaf40aa79c17edb
Jason Robitaille <jason.robitaille@lge.com>
2018-06-04 15:56:54 -0700

    Enact framework dependencies override script

--- node_binaries/enact-override.js
+++ node_binaries/enact-override.js
@@ -2,23 +2,32 @@ const path = require('path');
 const fs = require('fs');
 let override = process.argv[2];
 
-/*(if(path.isAbsolute(override)) {
+if(path.isAbsolute(override)) {
 	override = path.relative(process.cwd(), override);
-}*/
+}
 
-[/*'package.json', */'npm-shrinkwrap.json'].forEach(f => {
-	const obj = JSON.parse(fs.readFileSync(f, {encoding:'utf8'}));
-	['dependencies', 'devDependencies'].forEach(x => {
-		Object.keys(obj[x] || {}).forEach(key => {
-			if(fs.existsSync(path.join(override, ...key.split('/')))) {
-				if(f==='npm-shrinkwrap.json') {
-					obj[x][key].resolved = `file:${override}/${key}`;
-				// } else {
-				//	obj[x][key] = `file:${override}/${key}`;
+['package.json', 'package-lock.json',, 'npm-shrinkwrap.json'].forEach(f => {
+	if(fs.existsSync(f)) {
+		if(fs.existsSync(f + '.bak')) {
+			fs.unlinkSync(f);
+			fs.renameSync(f + '.bak', f);
+		}
+
+		const obj = JSON.parse(fs.readFileSync(f, {encoding:'utf8'}));
+		['dependencies', 'devDependencies'].forEach(x => {
+			Object.keys(obj[x] || {}).forEach(key => {
+				if(fs.existsSync(path.join(override, ...key.split('/'), 'package.tgz'))) {
+					if(typeof obj[x][key] === 'object') {
+						obj[x][key].resolved = `file:${override}/${key}/package.tgz`;
+						delete obj[x][key].integrity;
+					} else {
+						obj[x][key] = `file:${override}/${key}/package.tgz`;
+					}
 				}
-			}
+			});
 		});
-	})
-	fs.writeFileSync(f, JSON.stringify(obj, null, '  '), {encoding:'utf8'});
+		fs.renameSync(f, f + '.bak');
+		fs.writeFileSync(f, JSON.stringify(obj, null, '  '), {encoding:'utf8'});
+	}
 })
 
