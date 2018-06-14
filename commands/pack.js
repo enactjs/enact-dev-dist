fe70a9df7975fd2f90d70768475b56d0ba1764e9
Jason Robitaille <jason.robitaille@lge.com>
2018-06-14 16:26:16 -0700

    ENYO-5310: Add vebose logging support to Enact CLI pack

--- commands/pack.js
+++ commands/pack.js
@@ -55,6 +55,7 @@ function displayHelp() {
 	console.log('    -s, --snapshot    Generate V8 snapshot blob');
 	console.log('                      (requires V8_MKSNAPSHOT set)');
 	console.log('    --stats           Output bundle analysis file');
+	console.log('    --verbose         Verbose log build details');
 	console.log('    -v, --version     Display version information');
 	console.log('    -h, --help        Display help information');
 	console.log();
@@ -201,7 +202,7 @@ function api(opts = {}) {
 
 function cli(args) {
 	const opts = minimist(args, {
-		boolean: ['minify', 'framework', 'stats', 'production', 'isomorphic', 'snapshot', 'watch', 'help'],
+		boolean: ['minify', 'framework', 'stats', 'production', 'isomorphic', 'snapshot', 'verbose', 'watch', 'help'],
 		string: ['externals', 'externals-public', 'locales', 'output'],
 		default: {minify: true},
 		alias: {o: 'output', p: 'production', i: 'isomorphic', l: 'locales', s: 'snapshot', w: 'watch', h: 'help'}
