da94f70477e62bc6da5e5de4cf91308744f35747
Jason Robitaille <jason.robitaille@lge.com>
2018-06-18 13:37:25 -0700

    Fixes for Shrinkwrap override with NPM 5.1.0 and later.

--- node_binaries/enact-override.js
+++ node_binaries/enact-override.js
@@ -6,17 +6,26 @@ if(path.isAbsolute(override)) {
 	override = path.relative(process.cwd(), override);
 }
 
-function updateDep(meta, key) {
+function updateDep(meta, key, lockfile) {
 	if(fs.existsSync(path.join(override, ...key.split('/'), 'package.tgz'))) {
-		meta.dependencies[key] = meta.dependencies[key] || {};
-		meta.dependencies[key].resolved = `file:${override}/${key}/package.tgz`;
-		delete meta.dependencies[key].from;
-		delete meta.dependencies[key].integrity;
-		delete meta.dependencies[key].requires;
+		if(lockfile) {
+			meta.dependencies[key] = meta.dependencies[key] || {};
+			meta.dependencies[key].version = `file:${override}/${key}/package.tgz`;
+			delete meta.dependencies[key].resolved;
+			delete meta.dependencies[key].from;
+			delete meta.dependencies[key].integrity;
+			delete meta.dependencies[key].requires;
+		} else {
+			meta.dependencies = meta.dependencies || {};
+			if (meta.dependencies[key]) {
+				meta.dependencies[key] = `file:${override}/${key}/package.tgz`;
+			}
+		}
 	}
 }
 
-['package-lock.json', 'npm-shrinkwrap.json'].forEach(f => {
+['package.json', 'package-lock.json', 'npm-shrinkwrap.json'].forEach(f => {
+	const lockfile = f !== 'package.json';
 	if(fs.existsSync(f)) {
 		if(fs.existsSync(f + '.bak')) {
 			fs.unlinkSync(f);
@@ -24,15 +33,17 @@ function updateDep(meta, key) {
 		}
 
 		const obj = JSON.parse(fs.readFileSync(f, {encoding:'utf8'}));
-		obj.lockfileVersion = 1;
-		obj.requires = true;
+		if (lockfile) {
+			obj.lockfileVersion = 1;
+			obj.requires = true;
+		}
 		fs.readdirSync(override).forEach(dep => {
 			if(dep.startsWith('@')) {
 				fs.readdirSync(path.join(override, dep)).forEach(scoped => {
-					updateDep(obj, dep + '/' + scoped);
+					updateDep(obj, dep + '/' + scoped, lockfile);
 				});
 			} else {
-				updateDep(obj, dep);
+				updateDep(obj, dep, lockfile);
 			}
 		})
 		fs.renameSync(f, f + '.bak');
